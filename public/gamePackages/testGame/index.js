b36491abe33211d1b3a76ee69ee9d92bcd0e904e
nivdc <nivdc@live.com>
2021-03-07 18:59:32 +0800

    Pause the update for a while

---
+++ public/gamePackages/testGame/index.js
@@ -0,0 +1,37 @@
+let playerList=null
+
+$(document).ready(()=>{
+  $('#msg').submit(event => {
+        event.preventDefault()
+        let message = {message:`${$('#msg input').val()}`}
+        $.post(`room/${currentRoom}/game/message`,JSON.stringify(message))
+        $('#msg input').val("")
+    })
+})
+
+function init(){
+    getLobbyInitInfo()
+    SSEconnection.addEventListener('chatMessage',(event)=>{
+        let data = JSON.parse(event.data)
+        let html=$('#gameMessageList table').html()+`
+        <tr>
+            <td>${data.senderName}:</td>
+            <td>${data.message}</td>
+        </tr>`
+        $('#messageList table').html(html)
+    })
+}
+
+function updateChatInfo(chatInfo){
+    $('#playerList table').html('')
+    $.each(chatInfo,(index,userName)=>{
+        $('#playerList table').append(
+        `
+        <tr>
+            <td>${userName}</td>
+        </tr>
+        `
+        )
+    })
+
+}

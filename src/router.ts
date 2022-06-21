f4da5d39bd9c8ccc65c2562fd9fafe12c784d050
Nivdc <nivdc@live.com>
2022-06-21 10:10:09 +0800

    remove devlog, it's stupid. and some else.

--- src/router.ts
+++ src/router.ts
@@ -1,3 +1,29 @@
-export function router(req: Request): Response {
-    return new Response("Hello, World!");
+export async function router(req: Request): Promise<Response> {
+    const url = new URL(req.url)
+    if(url.pathname === '/'){
+        url.pathname = '/index.html'
+    }
+    console.log(`${req.method}->${url.pathname}`)
+
+
+    let res = await server(req)
+    if(res === null){
+        res = await sendStaticResource(url.pathname)
+    }
+
+    if (req.body) {
+        const body = await req.text()
+        console.log("Body:", body)
+    }
+
+    return res
+}
+
+function server(req: Request): Response | null{
+    return null
+}
+
+function sendStaticResource(path : string): Response{
+
+    return  new Response("Hello, World!")
 }
--- test/router_test.ts
+++ test/router_test.ts
@@ -1,3 +1,10 @@
-import { assertEquals } from "../models/dev_deps.ts";
+import { assertEquals } from "../models/dev_deps.ts"
 import { router } from "../src/router.ts"
 
+Deno.test('get index page test', async (t) => {
+    const res_body = "Hello, World!"
+    const real_res = await router(new Request('http://0.0.0.0/'))
+    const real_res_body = await real_res?.text()
+
+    assertEquals(real_res_body, res_body)
+})
